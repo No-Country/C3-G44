3cd68d62b09f59efb9d9487140e8fe0c5af70e17
jorgevelasquezv <jorgeluisvelasquezv@gmail.com>
2022-03-09 21:51:22 -0500

    Despliegue de front y back

--- client/src/index.js
+++ client/src/index.js
@@ -5,7 +5,7 @@ import App from './App.jsx';
 import Axios from 'axios'
 import reportWebVitals from './reportWebVitals';
 
-Axios.defaults.baseURL = 'http://localhost:4000/';
+Axios.defaults.baseURL = 'https://coderporfolioserver.herokuapp.com/';
 
 ReactDOM.render(
   <React.StrictMode>
